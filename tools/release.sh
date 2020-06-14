bbfa899de9c9a554b57a7b16771aa8e527e369cd
Raphaël Barrois <raphael.barrois@polytechnique.org>
2020-06-14 17:15:15 +0200

    Fix release script.

--- tools/release.sh
+++ tools/release.sh
@@ -6,7 +6,7 @@ REPO=filedist@dist.xelnor.net:pub/
 echo "** Latest tags:"
 git tag --list --sort taggerdate | tail -n 5
 
-echo -e "** Type in version number to release:\n> "
+echo -ne "** Type in version number to release:\n> "
 read version
 
 TAG_NAME=${PACKAGE_NAME}-${version}
@@ -16,6 +16,7 @@ git tag "${TAG_NAME}"
 
 ARCHIVE="dist/${TAG_NAME}.tar.gz"
 echo "** Exporting archive to ${ARCHIVE}"
+mkdir --parents dist/
 git archive --format=tar.gz --prefix="${TAG_NAME}/" --output="${ARCHIVE}" ${TAG_NAME}
 
 echo "** Uploading archive..."
